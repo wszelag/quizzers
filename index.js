e3bc8001627d55b4ded030d29177e3e2a08b198d
wszelag <wsz3lag@gmail.com>
2021-03-01 19:26:25 +0100

    add users router&post method to register

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const { json, urlencoded } = require("body-parser");
 const connectDB = require("./config/db");
+const usersRoutes = require("./routes/users");
 const PORT = 8000;
 
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: false }));
 
-connectDB();
+app.use("/users", require("./routes/users"));
+
 app.listen(PORT, () => console.log("server is listening"));
---
+++ routes/users.js
@@ -0,0 +1,53 @@
+const { Router } = require("express");
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
+
+const router = Router();
+
+router.post(
+  "/",
+  check("name", "Name is required").notEmpty(),
+  check("name", "Name should contain max. 10 characters").isLength({ max: 10 }),
+  check("email", "Please include a valid email").isEmail(),
+  check("password", "Password should contain 6 or more characters").isLength({
+    min: 6
+  }),
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      });
+    }
+    const { email, name, password } = req.body;
+    try {
+      const isUserAlreadyExists = await User.findOne({ email });
+
+      if (isUserAlreadyExists) {
+        return res.status(400).json({
+          errors: [{ msg: "User is already exist" }]
+        });
+      }
+
+      const user = new User({
+        name,
+        email,
+        password
+      });
+
+      await user.save();
+
+      res.status(200).json({
+        user
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        msg: "Server error"
+      });
+    }
+  }
+);
+
+module.exports = router;
