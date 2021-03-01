e104fb81bf2ea9192f2df7ca276b1e27eb97d57d
wszelag <wsz3lag@gmail.com>
2021-03-01 18:33:46 +0100

    connect to database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { json, urlencoded } = require("body-parser");
+const connectDB = require("./config/db");
 const PORT = 8000;
 
 const app = express();
@@ -8,4 +9,6 @@ const app = express();
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: false }));
+
+connectDB();
 app.listen(PORT, () => console.log("server is listening"));
