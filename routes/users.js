06445b36b8321980f9cf0c9600db3517d94def7d
wszelag <wsz3lag@gmail.com>
2021-03-02 19:10:27 +0100

    add delete account route

--- routes/users.js
+++ routes/users.js
@@ -95,4 +95,49 @@ router.post(
   }
 );
 
+//DELETE ACCOUNT
+router.delete(
+  "/delete",
+  check("email", "Please include a valid email").isEmail(),
+  check("password", "Password should contain 6 or more characters").isLength({
+    min: 6
+  }),
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      });
+    }
+
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({
+          errors: [{ msg: "User doesn't exist. Check your email." }]
+        });
+      }
+
+      if (user.password !== password) {
+        return res.status(400).json({
+          errors: [{ msg: "Invalid password, try again" }]
+        });
+      }
+
+      await User.deleteOne({ email });
+      res.status(200).json({
+        msg: "Your account has been deleted"
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        msg: "Server error"
+      });
+    }
+  }
+);
+
 module.exports = router;
