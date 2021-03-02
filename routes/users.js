049e46a9f5e31714b04066d3948a8a7dc63b35f3
wszelag <wsz3lag@gmail.com>
2021-03-02 19:17:15 +0100

    add change password route

--- routes/users.js
+++ routes/users.js
@@ -140,4 +140,48 @@ router.delete(
   }
 );
 
+//CHANGE PASSWORD
+router.patch(
+  "/change",
+  check("password", "Password should contain 6 or more characters").isLength({
+    min: 6
+  }),
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      });
+    }
+
+    const { email, password, newPassword } = req.body;
+    try {
+      const user = await User.findOne({ email });
+
+      if (user.password !== password) {
+        return res.status(400).json({
+          errors: [{ msg: "Invalid password, try again" }]
+        });
+      }
+
+      if (password === newPassword) {
+        return res.status(400).json({
+          errors: [{ msg: "Password are the same" }]
+        });
+      }
+
+      await User.updateOne({ email }, { password: newPassword });
+      res.status(200).json({
+        msg: "Your password has been changed"
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        msg: "Server error"
+      });
+    }
+  }
+);
+
 module.exports = router;
