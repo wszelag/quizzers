4db4bbb4bc58d3d4d29ddcb841fb4ed24ce70c54
wszelag <wsz3lag@gmail.com>
2021-03-03 17:38:12 +0100

    change folder name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Registration from "./components/Registration/Registration";
+import Registration from "./components/Auth/Registration";
 
 const App = () => {
   return (
